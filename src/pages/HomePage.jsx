a928004df9bbce6c3aa3cdea9a0eeaa1b14bf4a4
JohanSmirnoff <smirnoff.johan@gmail.com>
2025-12-09 11:18:14 +0100

    börjat på startsida

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,29 @@
+function HomePage () {
+    return(
+        <>
+            <header>
+                <nav>
+
+                </nav>
+                <h1>
+                    Productivity Assistant Application
+
+                </h1>
+
+            </header>
+            <main>
+                <div>
+                {/* todo */}
+                </div>
+                <div>
+                    {/* habit */}
+                </div> 
+                <div>
+                    {/* event  */}
+                </div>
+            </main>
+        </>
+    )
+}
+    
+export default HomePage
