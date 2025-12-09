aa95b0232e46befb71961c7736a43e133d745174
Milad <miladposhtkohi@gmail.com>
2025-12-09 13:34:09 +0100

    Navbar uppdater

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <div>
+    <div className="navbar">
+      <h1>Grupp 9 </h1>
+      <ul>
+        <li>
+         
+          Home
+        </li>
+        <li>
       
+          Todos & Activities
+        </li>
+        <li>
+       
+          Habits
+        </li>
+        <li>
+   
+          Event planner
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
